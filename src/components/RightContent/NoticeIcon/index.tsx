9a3af4b0ed9da853e4e8cd5be5a2440a57d9c7b3
nguyenhung252006 <nguyenviethungk56@gmail.com>
2026-05-14 20:58:56 +0700

    fix css + ui + fix get api notiiceIcon-get thong bao

--- src/components/RightContent/NoticeIcon/index.tsx
+++ src/components/RightContent/NoticeIcon/index.tsx
@@ -3,6 +3,7 @@ import { Modal } from 'antd';
 import { useEffect, useState } from 'react';
 import { useModel } from '@umijs/max';
 import NoticeIcon from './NoticeIcon';
+import styles from './index.less';
 
 const NoticeIconView = () => {
 	const {
@@ -20,10 +21,22 @@ const NoticeIconView = () => {
 	} = useModel('thongbao.noticeicon');
 	const [visibleDetail, setVisibleDetail] = useState<boolean>(false);
 	const [visiblePopup, setVisiblePopup] = useState<boolean>(false);
+	const [isShaking, setIsShaking] = useState<boolean>(false);	
 
 	useEffect(() => {
 		getThongBaoModel();
 	}, [page, limit]);
+
+	useEffect(() => {
+    // rung lần đầu tiên khi component được mount
+    // rung khi số lượng tin nhắn mới (unread) tăng lên
+    if (unread >= 0) {
+        setIsShaking(true);
+        const timer = setTimeout(() => setIsShaking(false), 600);
+        return () => clearTimeout(timer);
+    }
+    
+}, [unread]); 
 
 	const clearReadState = async () => {
 		readNotificationModel('ALL');
@@ -32,37 +45,39 @@ const NoticeIconView = () => {
 
 	return (
 		<>
-			<NoticeIcon
-				count={unread}
-				onItemClick={async (item) => {
-					setRecord(item);
-					setVisibleDetail(true);
-					setVisiblePopup(false);
-				}}
-				loading={loading}
-				onClear={() => clearReadState()}
-				clearText='Đánh dấu tất cả là đã đọc'
-				viewMoreText='Tải thêm'
-				onViewMore={() => {
-					if (loading) return;
-					setLimit(limit + 5);
-				}}
-				popupVisible={visiblePopup}
-				clearClose
-				onPopupVisibleChange={(visible) => {
-					setVisiblePopup(visible);
-				}}
-			>
-				<NoticeIcon.Tab
-					tabKey='notification'
-					count={total}
-					list={danhSach}
-					title='Thông báo'
-					emptyText='Bạn đã xem tất cả thông báo'
-					showClear={!!unread}
-					showViewMore={danhSach.length < total}
-				/>
-			</NoticeIcon>
+			<div className={`${styles.noticeWrapper} ${isShaking ? styles.bellIconShake : ''}`}>
+				<NoticeIcon
+					count={unread}
+					onItemClick={async (item) => {
+						setRecord(item);
+						setVisibleDetail(true);
+						setVisiblePopup(false);
+					}}
+					loading={loading}
+					onClear={() => clearReadState()}
+					clearText='Đánh dấu tất cả là đã đọc'
+					viewMoreText='Tải thêm'
+					onViewMore={() => {
+						if (loading) return;
+						setLimit(limit + 5);
+					}}
+					popupVisible={visiblePopup}
+					clearClose
+					onPopupVisibleChange={(visible) => {
+						setVisiblePopup(visible);
+					}}
+				>
+					<NoticeIcon.Tab
+						tabKey='notification'
+						count={total}
+						list={danhSach}
+						title='Thông báo'
+						emptyText='Bạn đã xem tất cả thông báo'
+						showClear={!!unread}
+						showViewMore={danhSach.length < total}
+					/>
+				</NoticeIcon>
+			</div>
 
 			<Modal
 				width={800}
